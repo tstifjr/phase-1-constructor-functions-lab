772601504b83a33c37464fd7f8b2897a97ee669b
TJ Stifter <tjstifter@gmail.com>
2023-06-09 09:36:52 -0400

    completes lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(arg1, arg2, arg3){
+    this.year = arg1;
+    this.color = arg2;
+    this.model = arg3
+}
+
+function Driver(arg1, arg2, arg3){
+    this.name = arg1;
+    this.age = arg2;
+    this.experience = arg3;
+}
+
+function PickupLocation(arg1, arg2){
+    this.address = arg1;
+    this.city = arg2;
+}
